ea83dd312c1fb3174bcc3a77a8fcd903fed75b1c
DraconDev <dracsharp@gmail.com>
2023-10-15 05:44:50 +0100

    add isInSlice

--- app.go
+++ app.go
@@ -19,11 +19,11 @@ func main() {
 	// fmt.Println(fizzBuzz(15))
 	// println(numberOfSteps(14))
 
-	node5 := &ListNode{Val: 5, Next: nil}
-	node4 := &ListNode{Val: 4, Next: node5}
-	node3 := &ListNode{Val: 3, Next: node4}
-	node2 := &ListNode{Val: 2, Next: node3}
-	node1 := &ListNode{Val: 1, Next: node2}
+	// node5 := &ListNode{Val: 5, Next: nil}
+	// node4 := &ListNode{Val: 4, Next: node5}
+	// node3 := &ListNode{Val: 3, Next: node4}
+	// node2 := &ListNode{Val: 2, Next: node3}
+	// node1 := &ListNode{Val: 1, Next: node2}
 
 	// node1 := &ListNode{Val: 1, Next: nil}
 	// result := middleNode(node1)
@@ -32,14 +32,16 @@ func main() {
 	// fmt.Println(GetSize(4, 2, 6))
 	// fmt.Println(Grow([]int{1, 2, 3, 4}))
 	// fmt.Println(MergeArrays([]int{1, 2, 3}, []int{4, 5, 6}))
-	fmt.Println(MergeArrays([]int{8, 44, 1, 2, 3, 3, 3}, []int{4, 5, 6, 8, 9, 12}))
+	// fmt.Println(MergeArrays([]int{8, 44, 1, 2, 3, 3, 3}, []int{4, 5, 6, 8, 9, 12}))
 	// fmt.Println(longestCommonPrefix([]string{"flower", "flow", "flight"}))
 	// fmt.Println(Strong(145))
 	// fmt.Println(PartList([]string{"a", "b", "c", "d"}))
 	// fmt.Println(OrderedCount("abracadabra"))
-	fmt.Println(removeNthFromEnd(node1, 2))
-	fmt.Println(bandNameGenerator("c-clamp"))
-	fmt.Println(bandNameGenerator("alaska"))
-	fmt.Println(reverseList(node1))
-	fmt.Println(Capitalize("hello", []int{0, 1, 2}))
+	// fmt.Println(removeNthFromEnd(node1, 2))
+	// fmt.Println(bandNameGenerator("c-clamp"))
+	// fmt.Println(bandNameGenerator("alaska"))
+	// fmt.Println(reverseList(node1))
+	// fmt.Println(Capitalize("hello", []int{0, 1, 2}))
+	// fmt.Println(Comp([]int{121, 144, 19, 161, 19, 144, 19, 11}, []int{121, 14641, 20736, 361, 25921, 361, 20736, 361}))
+	fmt.Println(Comp([]int{2, 3, 4, 5}, []int{9, 16, 25}))
 }
--- indexed_capital.go
+++ indexed_capital.go
@@ -1,6 +1,8 @@
 package main
 
-import "strings"
+import (
+	"strings"
+)
 
 func Capitalize(st string, arr []int) string {
 	//..
@@ -13,4 +15,27 @@ func Capitalize(st string, arr []int) string {
 		}
 	}
 	return string(slice)
+}
+
+func isInSlice(slice []int, val int) bool {
+	for _, v := range slice {
+		check := v * v
+		// check := int(math.Sqrt(float64(val)))
+		if check == val {
+			return true
+		}
+	}
+	return false
+}
+
+func Comp(array1 []int, array2 []int) bool {
+	if array1 == nil && array2 == nil {
+		return false
+	}
+	for _, v := range array2 {
+		if !isInSlice(array1, v) {
+			return false
+		}
+	}
+	return true
 }
