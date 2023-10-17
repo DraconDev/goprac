ddb5e7dc40fb9b22d66a7b846bc83e30a91b65ce
DraconDev <dracsharp@gmail.com>
2023-10-17 07:41:25 +0100

    add frequency in string

--- app.go
+++ app.go
@@ -19,11 +19,11 @@ func main() {
 	// fmt.Println(fizzBuzz(15))
 	// println(numberOfSteps(14))
 
-	node5 := &ListNode{Val: 1, Next: nil}
-	node4 := &ListNode{Val: 2, Next: node5}
-	node3 := &ListNode{Val: 3, Next: node4}
-	node2 := &ListNode{Val: 2, Next: node3}
-	node1 := &ListNode{Val: 1, Next: node2}
+	// node5 := &ListNode{Val: 1, Next: nil}
+	// node4 := &ListNode{Val: 2, Next: node5}
+	// node3 := &ListNode{Val: 3, Next: node4}
+	// node2 := &ListNode{Val: 2, Next: node3}
+	// node1 := &ListNode{Val: 1, Next: node2}
 
 	// noded := &ListNode{Val: 44, Next: nil}
 	// nodec := &ListNode{Val: 23, Next: noded}
@@ -68,6 +68,7 @@ func main() {
 	// fmt.Println(isValid("()[]{}"))
 	// fmt.Println(isValidBrackets("(]"))
 	// fmt.Println(CheckForFactor(4, 2))
-	fmt.Println(isPalindromeLinked(node1))
+	// fmt.Println(isPalindromeLinked(node1))
+	fmt.Println(FreqSeq("abracadabra", "b"))
 
 }
---
+++ frequency.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"strconv"
+	"strings"
+)
+
+func FreqSeq(str string, sep string) string {
+	arr := []string{}
+	for _, v := range str {
+		arr = append(arr, strconv.Itoa(strings.Count(str, string(v))))
+	}
+	result := strings.Join(arr, sep)
+	return result //Code goes here!
+}
