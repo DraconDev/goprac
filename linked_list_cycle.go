c2318cb3c7bf8ab0bab908cd2e0a019736e0b4c8
DraconDev <dracsharp@gmail.com>
2023-10-17 08:06:22 +0100

    add linked list cycle with floyd algorithm

--- linked_list_cycle.go
+++ linked_list_cycle.go
@@ -1,19 +1,40 @@
 package main
 
+// func hasCycle(head *ListNode) bool {
+// 	//
+// 	for head != nil {
+// 		if head.Next == nil {
+// 			return false
+// 		}
+// 		temp := head
+// 		for temp.Next != nil {
+// 			if temp.Next == head {
+// 				return true
+// 			}
+// 			temp = temp.Next
+// 		}
+// 		head = head.Next
+// 	}
+// 	return false
+// }
+
+
+// floyd algorithm
 func hasCycle(head *ListNode) bool {
-	//
-	for head != nil {
-		if head.Next == nil {
+	if head == nil || head.Next == nil {
+		return false
+	}
+
+	slow := head
+	fast := head.Next
+
+	for slow != fast {
+		if fast == nil || fast.Next == nil {
 			return false
 		}
-		temp := head
-		for temp.Next != nil {
-			temp = temp.Next
-			if temp == head {
-				return true
-			}
-		}
-		head = head.Next
+		slow = slow.Next
+		fast = fast.Next.Next
 	}
-	return false
+
+	return true
 }
