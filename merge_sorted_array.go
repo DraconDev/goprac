80f537ca7795b96349bed017f15741a7fd3c668d
DraconDev <dracsharp@gmail.com>
2023-10-23 10:14:08 +0100

    upd removed needless part from merge

--- merge_sorted_array.go
+++ merge_sorted_array.go
@@ -15,10 +15,10 @@ package main
 
 func Merge(nums1 []int, m int, nums2 []int, n int) {
 	var i, j = m - 1, n - 1
-	length := m + n
+	length := m + n -1
 	for j >= 0 {
 		if i >= 0 && nums1[i] > nums2[j] {
-			nums1[length-1], nums1[i] = nums1[i], nums1[length-1]
+			nums1[length-1]  = nums1[i]
 			i--
 		} else {
 			nums1[length-1] = nums2[j]
