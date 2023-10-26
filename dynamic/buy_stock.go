1b3bfcac0d06bab7e06eaabf31e81bd50c4e165f
DraconDev <dracsharp@gmail.com>
2023-10-26 12:29:15 +0100

    add profit with pointers

--- dynamic/buy_stock.go
+++ dynamic/buy_stock.go
@@ -1,16 +1,29 @@
 package dynamic
 
-import "sort"
+// func MaxProfit(prices []int) int {
+// 	profit := make([]int, len(prices))
+// 	for i := 0; i < len(prices); i++ {
+// 		for j := i + 1; j < len(prices); j++ {
+// 			if prices[j]-prices[i] > profit[i] {
+// 				profit[i] = prices[j] - prices[i]
+// 			}
+// 		}
+// 	}
+// 	sort.Ints(profit)
+// 	return profit[len(profit)-1]
+// }
 
 func MaxProfit(prices []int) int {
-	profit := make([]int, len(prices))
-	for i := 0; i < len(prices); i++ {
-		for j := i + 1; j < len(prices); j++ {
-			if prices[j]-prices[i] > profit[i] {
-				profit[i] = prices[j] - prices[i]
-			}
+	var l, r = 0, 1
+	profit := 0
+	for r < len(prices) {
+		if prices[r]-prices[l] > profit {
+			profit = prices[r] - prices[l]
 		}
+		if prices[r]-prices[l] < 0 {
+			l = r
+		}
+		r++
 	}
-	sort.Ints(profit)
-	return profit[len(profit)-1]
+	return profit
 }
