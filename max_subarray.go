989cb5cfb807472872eb204aa5c3bc00c2d301ff
DraconDev <dracsharp@gmail.com>
2023-10-27 11:42:28 +0100

    add max sub array better solution

--- max_subarray.go
+++ max_subarray.go
@@ -27,21 +27,16 @@ func maxSubArray(nums []int) int {
 	if len(nums) == 1 {
 		return nums[0]
 	}
-	index := 1
 	maxSum := nums[0]
-	currentSub := nums[0]
-	for index < len(nums) {
-		currentSub += nums[index]
-		if currentSub > maxSum && currentSub > nums[index] {
+	currentSub := 0
+	for _, e := range nums {
+		currentSub += e
+		if currentSub > maxSum {
 			maxSum = currentSub
-		} else if nums[index] > maxSum {
-			maxSum = nums[index]
-			currentSub = nums[index]
 		}
 		if currentSub < 0 {
 			currentSub = 0
 		}
-		index++
 	}
 	return maxSum
 }
