32378b20ca311f30f25fd70b0541f06b602d5f5c
DraconDev <dracsharp@gmail.com>
2023-11-02 14:19:38 +0000

    add ispowerofthree loop

--- powerofthree.go
+++ powerofthree.go
@@ -1,11 +1,20 @@
 package main
 
+// func isPowerOfThree(n int) bool {
+// 	if n == 3 {
+// 		return true
+// 	}
+// 	if n%3 == 0 {
+// 		return isPowerOfThree(n / 3)
+// 	}
+// 	return false
+// }
+
 func isPowerOfThree(n int) bool {
-	if n == 3 {
-		return true
-	}
-	if n%3 == 0 {
-		return isPowerOfThree(n / 3)
+	for i := 3; i < n; i *= 3 {
+		if i == n {
+			return true
+		}
 	}
 	return false
 }
