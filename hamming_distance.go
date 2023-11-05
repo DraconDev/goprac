2a37ff30bcc7370ca60ff993bbac713dbcaf5788
DraconDev <dracsharp@gmail.com>
2023-11-05 09:02:21 +0000

    add bitwise solution

--- hamming_distance.go
+++ hamming_distance.go
@@ -1,9 +1,5 @@
 package main
 
-import (
-	"strconv"
-)
-
 // func HammingDistance(x int, y int) int {
 // 	convertedX := strconv.FormatInt(int64(x), 2)
 // 	convertedY := strconv.FormatInt(int64(y), 2)
@@ -25,24 +21,34 @@ import (
 // 	return difference
 // }
 
-func HammingDistance(x int, y int) int {
-	short := strconv.FormatInt(int64(x), 2)
-	long := strconv.FormatInt(int64(y), 2)
-	if len(short) > len(long) {
-		short, long = long, short
-	}
-	difference := 0
-	for i := 0; i < len(long)-len(short); i++ {
-		if long[i] == '1' {
-			difference++
-		}
-	}
-	long = long[len(long)-len(short):]
-	for i, v := range long {
-		if v != rune(short[i]) {
-			difference++
-		}
+// func HammingDistance(x int, y int) int {
+// 	short := strconv.FormatInt(int64(x), 2)
+// 	long := strconv.FormatInt(int64(y), 2)
+// 	if len(short) > len(long) {
+// 		short, long = long, short
+// 	}
+// 	difference := 0
+// 	for i := 0; i < len(long)-len(short); i++ {
+// 		if long[i] == '1' {
+// 			difference++
+// 		}
+// 	}
+// 	long = long[len(long)-len(short):]
+// 	for i, v := range long {
+// 		if v != rune(short[i]) {
+// 			difference++
+// 		}
 
+// 	}
+// 	return difference
+// }
+
+func HammingDistance(x, y int) int {
+	var distance int
+	for x != 0 || y != 0 {
+		x &= x - 1
+		y &= y - 1
+		distance++
 	}
-	return difference
+	return distance
 }
