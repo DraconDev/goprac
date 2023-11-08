0cd819604e9f42c7a05a094a4ec136433af07a9a
DraconDev <dracsharp@gmail.com>
2023-11-08 19:50:11 +0000

    upd test name

--- main_test.go
+++ main_test.go
@@ -12,9 +12,13 @@ func Test(t *testing.T) {
 	// assert.Equal(t, ReverseBits(43261596), 964176192)
 	// assert.Equal(t, leetcode.Pascals_triangle(2), [][]int{{1}, {1, 1}})
 	// assert.Equal(t, leetcode.Pascals_triangle(5), [][]int{{1}, {1, 1}, {1, 2, 1}, {1, 3, 3, 1}, {1, 4, 6, 4, 1}})
+
+	// assert.Equal(t, HammingDistance(1, 5), 2)
+
+}
+
+func TestAnagram(t *testing.T) {
 	assert.Equal(t, leetcode.Anagram("anagram", "nagaram"), true)
 	assert.Equal(t, leetcode.Anagram("anagram", "nagoram"), false)
 	assert.Equal(t, leetcode.Anagram("anagram", "nagram"), false)
-	// assert.Equal(t, HammingDistance(1, 5), 2)
-
 }
