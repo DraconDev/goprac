a3dd1356424829600ed446f1475246155c5685f6
DraconDev <dracsharp@gmail.com>
2023-11-15 10:12:52 +0000

    upd ReverseWords

--- leetcode/reverse_words.go
+++ leetcode/reverse_words.go
@@ -2,9 +2,17 @@ package leetcode
 
 import "strings"
 
+// func ReverseWords(s string) string {
+// 	trimmedStr := strings.TrimSpace(s)
+// 	result := strings.Split(trimmedStr, " ")
+// 	for i := 0; i < len(result)/2; i++ {
+// 		result[i], result[len(result)-i-1] = result[len(result)-i-1], result[i]
+// 	}
+// 	return strings.Join(result, " ")
+// }
+
 func ReverseWords(s string) string {
-	trimmedStr := strings.TrimSpace(s)
-	result := strings.Split(trimmedStr, " ")
+	result := strings.Fields(s)
 	for i := 0; i < len(result)/2; i++ {
 		result[i], result[len(result)-i-1] = result[len(result)-i-1], result[i]
 	}
