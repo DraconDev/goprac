131b1e77c2ee8d8baa3f0489979646df8b317c1e
DraconDev <dracsharp@gmail.com>
2023-11-15 11:00:21 +0000

    add TwoSum pointer solution

--- leetcode/twosum.go
+++ leetcode/twosum.go
@@ -15,17 +15,32 @@ package leetcode
 // 	return nil
 // }
 
-func TwoSum(numbers []int, target int) []int {
-	for i := 0; i < len(numbers); i++ {
-		for j := len(numbers)-1; j > i; j-- {
-			if numbers[i] + numbers[j] > target {
-				numbers = numbers[:j]
-				continue
-			}
-			if numbers[i] + numbers[j] == target {
-				return []int{i+1, j+1}
-			}
+// func TwoSum(numbers []int, target int) []int {
+// 	for i := 0; i < len(numbers); i++ {
+// 		for j := len(numbers)-1; j > i; j-- {
+// 			if numbers[i] + numbers[j] > target {
+// 				numbers = numbers[:j]
+// 				continue
+// 			}
+// 			if numbers[i] + numbers[j] == target {
+// 				return []int{i+1, j+1}
+// 			}
 
+// 		}
+// 	}
+// 	return nil
+// }
+
+func TwoSum(numbers []int, target int) []int {
+	var l, r = 0, len(numbers) - 1
+	for l < r {
+		sum := numbers[l] + numbers[r]
+		if sum == target {
+			return []int{l + 1, r + 1}
+		} else if sum < target {
+			l++
+		} else {
+			r--
 		}
 	}
 	return nil
