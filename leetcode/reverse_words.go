3e598abf14bea8561efe1230a1418b3d53446cc8
DraconDev <dracsharp@gmail.com>
2023-11-15 10:11:36 +0000

    upd ReverseWords add trim

--- leetcode/reverse_words.go
+++ leetcode/reverse_words.go
@@ -3,7 +3,8 @@ package leetcode
 import "strings"
 
 func ReverseWords(s string) string {
-	result := strings.Split(s, " ")
+	trimmedStr := strings.TrimSpace(s)
+	result := strings.Split(trimmedStr, " ")
 	for i := 0; i < len(result)/2; i++ {
 		result[i], result[len(result)-i-1] = result[len(result)-i-1], result[i]
 	}
