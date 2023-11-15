4440c288a71390c107285ec595d18792b024542a
DraconDev <dracsharp@gmail.com>
2023-11-15 20:43:08 +0000

    upd IncreasingTriplet

--- leetcode/IncreasingTriplet.go
+++ leetcode/IncreasingTriplet.go
@@ -1,16 +1,36 @@
 package leetcode
 
+// func IncreasingTriplet(nums []int) bool {
+// 	if len(nums) < 3 {
+// 		return false
+// 	}
+// 	for i := 0; i < len(nums)-2; i++ {
+// 		for j := i + 1; j < len(nums)-1; j++ {
+// 			for k := j + 1; k < len(nums); k++ {
+// 				if nums[i] < nums[j] && nums[j] < nums[k] {
+// 					return true
+// 				}
+// 			}
+// 		}
+// 	}
+// 	return false
+// }
+
+import "math"
+
 func IncreasingTriplet(nums []int) bool {
 	if len(nums) < 3 {
 		return false
 	}
-	for i := 0; i < len(nums)-2; i++ {
-		for j := i + 1; j < len(nums)-1; j++ {
-			for k := j + 1; k < len(nums); k++ {
-				if nums[i] < nums[j] && nums[j] < nums[k] {
-					return true
-				}
-			}
+	small := math.MaxInt32
+	big := math.MaxInt32
+	for _, n := range nums {
+		if n <= small {
+			small = n
+		} else if n <= big {
+			big = n
+		} else {
+			return true
 		}
 	}
 	return false
