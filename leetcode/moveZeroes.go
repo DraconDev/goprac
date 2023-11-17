9c1ca45ef732ad2a7b8eb494ade3f76b9ce06a5a
DraconDev <dracsharp@gmail.com>
2023-11-17 06:07:40 +0000

    add MoveZeroes pointer

--- leetcode/moveZeroes.go
+++ leetcode/moveZeroes.go
@@ -1,18 +1,29 @@
 package leetcode
 
-func MoveZeroes(nums []int) {
-	result := []int{}
+// func MoveZeroes(nums []int) {
+// 	result := []int{}
 
-	for _, v := range nums {
+// 	for _, v := range nums {
+// 		if v != 0 {
+// 			result = append(result, v)
+// 		}
+// 	}
+// 	for i := range nums {
+// 		if i < len(result) {
+// 			nums[i] = result[i]
+// 		} else {
+// 			nums[i] = 0
+// 		}
+// 	}
+// }
+
+func MoveZeroes(nums []int) {
+	zeroIndex := 0
+	for i, v := range nums {
 		if v != 0 {
-			result = append(result, v)
+			nums[i], nums[zeroIndex] = nums[zeroIndex], nums[i]
+			zeroIndex++
 		}
 	}
-	for i := range nums {
-		if i < len(result) {
-			nums[i] = result[i]
-		} else {
-			nums[i] = 0
-		}
-	}
+	// return nums
 }
