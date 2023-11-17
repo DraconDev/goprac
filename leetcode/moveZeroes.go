35e54bc402260d7d790f77a74e72b55103485ac8
DraconDev <dracsharp@gmail.com>
2023-11-17 06:19:15 +0000

    add MoveZeroes 3rd solution

--- leetcode/moveZeroes.go
+++ leetcode/moveZeroes.go
@@ -17,13 +17,38 @@ package leetcode
 // 	}
 // }
 
+// func MoveZeroes(nums []int) []int {
+// 	zeroIndex := 0
+// 	for i, v := range nums {
+// 		if v != 0 {
+// 			nums[i], nums[zeroIndex] = nums[zeroIndex], nums[i]
+// 			zeroIndex++
+// 		}
+// 		if i > len(nums)-zeroIndex {
+// 			break
+// 		}
+// 	}
+// 	return nums
+// }
+
+// func MoveZeroes(nums []int) []int {
+// 	zeroIndex := 0
+// 	for i, v := range nums {
+// 		if v != 0 {
+// 			nums[i], nums[zeroIndex] = nums[zeroIndex], nums[i]
+// 			zeroIndex++
+// 		}
+// 	}
+// 	return nums
+// }
+
 func MoveZeroes(nums []int) {
-	zeroIndex := 0
-	for i, v := range nums {
-		if v != 0 {
-			nums[i], nums[zeroIndex] = nums[zeroIndex], nums[i]
-			zeroIndex++
+	snowBallSize := 0
+	for i := 0; i < len(nums); i++ {
+		if nums[i] == 0 {
+			snowBallSize++
+		} else if snowBallSize > 0 {
+			nums[i], nums[i-snowBallSize] = nums[i-snowBallSize], nums[i]
 		}
 	}
-	// return nums
 }
