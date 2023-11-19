02448e99977ee741581e2c09483893aec123a84d
DraconDev <dracsharp@gmail.com>
2023-11-19 07:07:28 +0000

    add IsValidParans v2

--- leetcode/valid_paranthesis.go
+++ leetcode/valid_paranthesis.go
@@ -1,22 +1,50 @@
 package leetcode
 
+// func IsValidParans(check string) bool {
+// 	if len(check)%2 == 1 || check == "" {
+// 		return false
+// 	}
+// 	brackets := map[byte]byte{
+// 		'(': ')',
+// 		'[': ']',
+// 		'{': '}',
+// 	}
+// 	stack := []byte{}
+// 	for i := 0; i < len(check); i++ {
+// 		for k, v := range brackets {
+// 			if check[i] == k {
+// 				stack = append(stack, check[i])
+// 				break
+// 			}
+// 			if check[i] == v {
+// 				if len(stack) == 0 || stack[len(stack)-1] != k {
+// 					return false
+// 				}
+// 				stack = stack[:len(stack)-1]
+// 				break
+// 			}
+// 		}
+// 	}
+// 	return len(stack) == 0
+// }
+
 func IsValidParans(check string) bool {
 	if len(check)%2 == 1 || check == "" {
 		return false
 	}
-	brackets := map[byte]byte{
+	brackets := map[rune]rune{
 		'(': ')',
 		'[': ']',
 		'{': '}',
 	}
-	stack := []byte{}
+	stack := []rune{}
 	for i := 0; i < len(check); i++ {
 		for k, v := range brackets {
-			if check[i] == k {
-				stack = append(stack, check[i])
+			if rune(check[i]) == k {
+				stack = append(stack, rune(check[i]))
 				break
 			}
-			if check[i] == v {
+			if rune(check[i]) == v {
 				if len(stack) == 0 || stack[len(stack)-1] != k {
 					return false
 				}
