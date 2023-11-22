3270b5f857cefbc10de107bff598f6bb795a3a39
DraconDev <dracsharp@gmail.com>
2023-11-22 08:45:24 +0000

    add LongestOnes extra code

--- leetcode/longestOnes.go
+++ leetcode/longestOnes.go
@@ -13,29 +13,3 @@ func LongestOnes(nums []int, k int) int {
 	}
 	return end - start
 }
-
-// func LongestOnes(nums []int, k int) int {
-// 	max := 0
-// 	currentMax := 0
-// 	currentK := k
-// 	for i := 0; i < len(nums); i++ {
-// 		if nums[i] == 1 {
-// 			currentMax++
-// 		} else {
-// 			if currentK > 0 {
-// 				currentMax++
-// 				currentK--
-// 			} else {
-// 				if currentMax > max {
-// 					max = currentMax
-// 				}
-// 				currentMax = k
-// 				// currentK = k
-// 			}
-// 		}
-// 	}
-// 	if max < currentMax {
-// 		max = currentMax
-// 	}
-// 	return max
-// }
