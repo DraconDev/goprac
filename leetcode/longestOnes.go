a55f692b5b9d2788fdb34389c260a3ee4b4ff2aa
DraconDev <dracsharp@gmail.com>
2023-11-22 09:13:30 +0000

    add LongestOnes better name

--- leetcode/longestOnes.go
+++ leetcode/longestOnes.go
@@ -1,19 +1,21 @@
 package leetcode
 
 func LongestOnes(nums []int, k int) int {
-	start := 0
-	end := 0
-	for end < len(nums) {
-		if nums[end] == 0 {
+	front := 0
+	back := 0
+	for back < len(nums) {
+		if nums[back] == 0 {
 			k--
 		}
 		if k < 0 {
-			if nums[start] == 0 {
+			if nums[front] == 0 {
 				k++
 			}
-			start++
+			front++
 		}
-		end++
+		back++
 	}
-	return end - start
+	return back - front
 }
+
+//
