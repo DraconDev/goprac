d8d317f53adcf09aec2c2fe5bac788e3eae28c50
DraconDev <dracsharp@gmail.com>
2023-11-24 15:09:04 +0000

    add CarFleet 2

--- leetcode/carfleet.go
+++ leetcode/carfleet.go
@@ -1,52 +1,56 @@
 package leetcode
 
-import "sort"
-
-func CarFleet(target int, position []int, speed []int) int {
-	timeArr := map[int]float64{}
-
-	for i := 0; i < len(position); i++ {
-		timeArr[position[i]] = float64(target-position[i]) / float64(speed[i])
-	}
-
-	sort.Slice(position, func(i, j int) bool {
-		return position[i] < position[j]
-	})
-
-	for i := 0; i < len(position)-1; i++ {
-		for j := i + 1; j <= len(position)-1; j++ {
-			if timeArr[position[i]] <= timeArr[position[j]] {
-				delete(timeArr, position[i])
-				break
-			}
-		}
-	}
-
-	return len(timeArr)
-}
+import (
+	"sort"
+)
 
 // func CarFleet(target int, position []int, speed []int) int {
-// 	timeArr := make([]float64, len(position))
+// 	timeArr := map[int]float64{}
 
 // 	for i := 0; i < len(position); i++ {
-// 		timeArr[i] = float64(target-position[i]) / float64(speed[i])
+// 		timeArr[position[i]] = float64(target-position[i]) / float64(speed[i])
 // 	}
 
 // 	sort.Slice(position, func(i, j int) bool {
 // 		return position[i] < position[j]
 // 	})
 
-// 	fleetCount := 0
-// 	maxTime := 0.0
-
-// 	for i := 0; i < len(position); i++ {
-// 		if timeArr[i] > maxTime {
-// 			fleetCount++
-// 			maxTime = timeArr[i]
-// 		} else {
-// 			timeArr[i] = maxTime
+// 	for i := 0; i < len(position)-1; i++ {
+// 		for j := i + 1; j <= len(position)-1; j++ {
+// 			if timeArr[position[i]] <= timeArr[position[j]] {
+// 				delete(timeArr, position[i])
+// 				break
+// 			}
 // 		}
 // 	}
 
-// 	return fleetCount
+// 	return len(timeArr)
 // }
+
+func CarFleet(target int, position []int, speed []int) int {
+	timeMap := make(map[int]float64)
+
+	for i := 0; i < len(position); i++ {
+		timeMap[-position[i]] = float64(target-position[i]) / float64(speed[i])
+	}
+
+	fleetCount := 0
+	maxTime := 0.0
+
+	keys := make([]int, len(timeMap))
+	i := 0
+	for k := range timeMap {
+		keys[i] = k
+		i++
+	}
+	sort.Ints(keys)
+
+	for _, k := range keys {
+		if timeMap[k] > maxTime {
+			maxTime = timeMap[k]
+			fleetCount++
+		}
+	}
+
+	return fleetCount
+}
