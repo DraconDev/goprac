f60572c1eec83704b87e8618f00204dc39fbc15f
DraconDev <dracsharp@gmail.com>
2024-02-05 08:36:25 +0000

    add 387. First Unique Character in a String

---
+++ leetcode/firstUniqChar.go
@@ -0,0 +1,14 @@
+package leetcode
+
+func firstUniqChar(s string) int {
+	m := make(map[rune]int)
+	for _, v := range s {
+		m[v]++
+	}
+	for i, v := range s {
+		if m[v] == 1 {
+			return i
+		}
+	}
+	return -1
+}
