ed6d68e64fbbd59096a10b3fe1224455a334fa5c
DraconDev <dracsharp@gmail.com>
2024-02-20 07:39:35 +0000

    add missingNumber

---
+++ leetcode/missingNumber.go
@@ -0,0 +1,29 @@
+package leetcode
+
+import "sort"
+
+func missingNumber(nums []int) int {
+	sort.Ints(nums)
+
+	for i, v := range nums {
+		if v != i {
+			return i
+		}
+	}
+	return nums[len(nums)-1] + 1
+}
+
+// 	dict := make([]bool, len(nums)+1)
+
+// 	for _, n := range nums {
+// 		dict[n] = true
+// 	}
+
+// 	for i, exists := range dict {
+// 		if !exists {
+// 			return i
+// 		}
+// 	}
+
+// 	return 0
+// }
