5a8a84985719cedd35e0fd21c4a817f846fe2bba
DraconDev <dracsharp@gmail.com>
2024-02-26 09:14:43 +0000

    add same tree

---
+++ tests/leetcode/tree/sametree.go
@@ -0,0 +1,18 @@
+package tree
+
+func isSameTree(p *TreeNode, q *TreeNode) bool {
+
+	var dfs func(p *TreeNode, q *TreeNode) bool
+	dfs = func(p *TreeNode, q *TreeNode) bool {
+		if p == nil && q == nil {
+			return true
+		} else if p == nil || q == nil {
+			return false
+		} else if p.Val != q.Val {
+			return false
+		}
+		return dfs(p.Left, q.Left) && dfs(p.Right, q.Right)
+	}
+	return dfs(p, q)
+
+}
