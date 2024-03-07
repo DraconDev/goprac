e1261c26ef220f8d1332e3839f43eaf39dfd648a
DraconDev <dracsharp@gmail.com>
2024-03-07 07:35:05 +0000

    add middleLinked

---
+++ linkedlist/middleOfLinkedList.go
@@ -0,0 +1,10 @@
+package linkedList
+
+func middleLinked(head *ListNode) *ListNode {
+	slow, fast := head, head
+	for fast != nil && fast.Next != nil {
+		slow = slow.Next
+		fast = fast.Next.Next
+	}
+	return slow
+}
