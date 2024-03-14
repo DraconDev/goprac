36368e7d22f62e2c91f0ccd272a06dd97f1bc679
DraconDev <dracsharp@gmail.com>
2024-03-14 12:46:01 +0000

    upd

--- leetcode/numSubarray.go
+++ leetcode/numSubarray.go
@@ -5,17 +5,40 @@ func NumSubarraysWithSum(nums []int, goal int) int {
 	var sum int
 	var curSum int
 
-	for i, _ := range nums {
+	for i := range nums {
 		for _, v2 := range nums[i:] {
 			curSum += v2
 			if curSum == goal {
 				sum++
-				curSum = 0
+			} else if curSum > goal {
+				break
 			}
-
 		}
+		curSum = 0
 	}
 
 	return sum
 
+}
+
+func sumAtMost(nums []int, goal int) int {
+	if goal < 0 {
+		return 0
+	}
+
+	res, prev, cur := 0, 0, 0
+	for i, val := range nums {
+		cur += val
+		for cur > goal {
+			cur -= nums[prev]
+			prev++
+		}
+		res += i - prev + 1
+	}
+
+	return res
+}
+
+func numSubarraysWithSum(nums []int, goal int) int {
+	return sumAtMost(nums, goal) - sumAtMost(nums, goal-1)
 }
--- tests/leetcode/numSubarray_test.go
+++ tests/leetcode/numSubarray_test.go
@@ -8,5 +8,7 @@ import (
 )
 
 func TestNumSubarraysWithSum(t *testing.T) {
-	assert.Equal(t, leetcode.NumSubarraysWithSum([]int{1, 0, 1, 0, 1}, 2), 4)
+	// assert.Equal(t, leetcode.NumSubarraysWithSum([]int{1, 0, 1, 0, 1}, 2), 4)
+	assert.Equal(t, leetcode.NumSubarraysWithSum([]int{0, 0, 0, 0, 1}, 2), 0)
+
 }
