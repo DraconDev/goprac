e7a2c5d05890739e8b2f1dfc101f2d26b78bef7e
DraconDev <dracsharp@gmail.com>
2024-03-26 10:37:14 +0000

    upd

--- leetcode/findDisappearedNumbers.go
+++ leetcode/findDisappearedNumbers.go
@@ -5,6 +5,10 @@ func findDisappearedNumbers(nums []int) []int {
 	numbers := make(map[int]bool)
 	length := len(nums)
 
+	if length == 0 {
+		return []int{}
+	}
+
 	for _, v := range nums {
 		numbers[v] = true
 	}
