39a4473b0eadc54b06c0324618289c94e87458b0
DraconDev <dracsharp@gmail.com>
2024-04-05 05:17:54 +0100

    add makeGood

---
+++ leetcode/makeGood.go
@@ -0,0 +1,25 @@
+package leetcode
+
+func makeGood(s string) string {
+
+	length := len(s)
+	i := 0
+	for i < length {
+		if i+1 < length && abs(int(s[i])-int(s[i+1])) == 32 {
+			s = s[:i] + s[i+2:]
+			length -= 2
+			i = 0
+		} else {
+			i++
+		}
+	}
+	return s
+
+}
+
+func abs(x int) int {
+	if x < 0 {
+		return -x
+	}
+	return x
+}
