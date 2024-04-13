4612773600af497248a9572f63f2a3dccec526ba
DraconDev <dracsharp@gmail.com>
2024-04-13 13:29:46 +0100

    add ConstructRectangle

--- leetcode/constructRectangle.go
+++ leetcode/constructRectangle.go
@@ -2,17 +2,25 @@ package leetcode
 
 import "math"
 
-func ConstructRectangle(area int) []int {
-	// square root area
-	width := int(math.Sqrt(float64(area)))
-	height := int(math.Sqrt(float64(area)))
+// func ConstructRectangle(area int) []int {
+// 	// square root area
+// 	width := int(math.Sqrt(float64(area)))
+// 	height := int(math.Sqrt(float64(area)))
 
-	for width*height != area {
-		if width*height < area {
-			width++
-		} else {
-			height--
-		}
+// 	for width*height != area {
+// 		if width*height < area {
+// 			width++
+// 		} else {
+// 			height--
+// 		}
+// 	}
+// 	return []int{width, height}
+// }
+
+func ConstructRectangle(area int) []int {
+	sqrt := int(math.Sqrt(float64(area)))
+	for area%sqrt != 0 {
+		sqrt--
 	}
-	return []int{width, height}
+	return []int{area / sqrt, sqrt}
 }
