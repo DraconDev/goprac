5f7cf52e10344a271f22d7e46301706e874cdeaa
DraconDev <dracsharp@gmail.com>
2024-04-25 07:11:23 +0100

    add findLUSlength

---
+++ leetcode/findUSLength.go
@@ -0,0 +1,23 @@
+package leetcode
+
+func findLUSlength(a string, b string) int {
+
+	if a == b {
+		return -1
+	}
+
+	var max int
+	var currentMax int
+	for i := 0; i < len(a); i++ {
+		if a[i] == b[i] {
+			currentMax++
+		} else {
+			if currentMax > max {
+				max = currentMax
+			}
+			currentMax = 0
+		}
+	}
+
+	return max
+}
