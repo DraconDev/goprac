e4299aa4e94f8c9de5995e2f379347fd1554164e
DraconDev <dracsharp@gmail.com>
2024-05-21 12:02:30 +0100

    add subsets

---
+++ leetcode/subsets.go
@@ -0,0 +1,11 @@
+package leetcode
+
+func subsets(nums []int) [][]int {
+	subsets := [][]int{{}}
+	for _, num := range nums {
+		for _, subset := range subsets {
+			subsets = append(subsets, append([]int{}, append(subset, num)...))
+		}
+	}
+	return subsets
+}
