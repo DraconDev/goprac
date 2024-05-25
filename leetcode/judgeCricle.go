5ae50391efc92435673d60d5b849b5b0f79f2f5b
DraconDev <dracsharp@gmail.com>
2024-05-25 06:05:48 +0100

    add judgeCircle

---
+++ leetcode/judgeCricle.go
@@ -0,0 +1,19 @@
+package leetcode
+
+func judgeCircle(moves string) bool {
+
+	x, y := 0, 0
+	for _, v := range moves {
+		switch v {
+		case 'U':
+			y++
+		case 'D':
+			y--
+		case 'L':
+			x--
+		case 'R':
+			x++
+		}
+	}
+	return x == 0 && y == 0
+}
