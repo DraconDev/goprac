54df9208a81997b1719420accad44d8f01d9e3bb
DraconDev <dracsharp@gmail.com>
2024-06-04 07:00:18 +0100

    add CountBinarySubstrings

--- leetcode/countBinarySubstring.go
+++ leetcode/countBinarySubstring.go
@@ -1,56 +1,17 @@
 package leetcode
 
 func CountBinarySubstrings(s string) int {
-	subStrings := make(map[string]int)
-
-	for i := range s {
-		count := 0
-
-		flip := struct {
-			c rune
-			f bool
-		}{'0', false}
-
-		for j, c := range s[i:] {
-
-			if c == '0' {
-				if j == 0 {
-					flip.c = '0'
-					flip.f = false
-				}
-				if flip.c == '1' {
-					flip.f = true
-				}
-				if flip.c == '0' && flip.f {
-					break
-				}
-
-				count--
-			} else {
-				if j == 0 {
-					flip.c = '1'
-					flip.f = false
-				}
-				if flip.c == '0' {
-					flip.f = true
-				}
-				if flip.c == '1' && flip.f {
-					break
-				}
-				count++
-			}
-			if count == 0 {
-				subStrings[s[i:i+j+1]]++
-				break
-			}
+	prev, cur, res := 0, 1, 0
+	for i := 1; i < len(s); i++ {
+		if s[i] == s[i-1] {
+			cur++
+		} else {
+			prev = cur
+			cur = 1
 		}
-
+		if prev >= cur {
+			res++
+		}
 	}
-	result := 0
-
-	for _, v := range subStrings {
-		result += v
-	}
-	return result
-
+	return res
 }
