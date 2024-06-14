b5000a12bbf6ccfa39ea85a106bfe2365b289b70
DraconDev <dracsharp@gmail.com>
2024-06-14 12:42:06 +0100

    add MinIncrementForUnique

--- leetcode/minIncrement.go
+++ leetcode/minIncrement.go
@@ -1,5 +1,7 @@
 package leetcode
 
+import "sort"
+
 // func MinIncrementForUnique(nums []int) int {
 // 	elements := make(map[int]int)
 // 	for _, v := range nums {
@@ -27,27 +29,20 @@ package leetcode
 // }
 
 func MinIncrementForUnique(nums []int) int {
-	elements := make(map[int]int)
-	for _, v := range nums {
-		elements[v]++
-	}
-	increment := 0
-	for k, v := range elements {
-		if v == 1 {
-			continue
-		}
-		counter := 0
-		i := k
-		for elements[k] > 1 {
+	sort.Ints(nums)
+	increments := 0
 
-			for elements[i] > 0 {
-				counter++
-				i++
-			}
-			elements[i]++
-			increment += counter
-			elements[k]--
+	if len(nums) < 2 {
+		return increments
+	}
+
+	for i := range nums[1:] {
+		for nums[i+1] <= nums[i] {
+			nums[i+1]++
+			increments++
 		}
 	}
-	return increment
+
+	return increments
+
 }
