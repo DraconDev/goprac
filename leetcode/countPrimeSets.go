46e92440195289e16aa5f2b9d38c21f274f4c66e
DraconDev <dracsharp@gmail.com>
2024-06-26 11:19:30 +0100

    udp

--- leetcode/countPrimeSets.go
+++ leetcode/countPrimeSets.go
@@ -1,14 +1,19 @@
 package leetcode
 
+import "strconv"
+
 func countPrimeSetBits(left int, right int) int {
 	sets := 0
 	for i := left; i <= right; i++ {
 		bits := 0
-		for j := 0; j < 32; j++ {
-			if i&(1<<j) > 0 {
+		// convert i to binary
+		binarrNum := strconv.FormatInt(int64(i), 2)
+		for j := 0; j < len(binarrNum); j++ {
+			if binarrNum[j] == '1' {
 				bits++
 			}
 		}
+
 		if bits == 2 || bits == 3 || bits == 5 || bits == 7 || bits == 11 || bits == 13 || bits == 17 || bits == 19 {
 			sets++
 		}
