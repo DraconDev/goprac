53e962d761564f0f9e62c7a36853f92e591d36a6
DraconDev <dracsharp@gmail.com>
2024-07-28 07:03:48 +0100

    add sortArrayByParity 2

--- leetcode/sortArrayByParity.go
+++ leetcode/sortArrayByParity.go
@@ -1,15 +1,34 @@
 package leetcode
 
+// func sortArrayByParity(nums []int) []int {
+// 	odds := []int{}
+// 	evens := []int{}
+// 	for _, v := range nums {
+// 		if v%2 == 0 {
+// 			evens = append(evens, v)
+// 		} else {
+// 			odds = append(odds, v)
+// 		}
+// 	}
+// 	return append(evens, odds...)
+
+// }
+
 func sortArrayByParity(nums []int) []int {
-	odds := []int{}
-	evens := []int{}
-	for _, v := range nums {
-		if v%2 == 0 {
-			evens = append(evens, v)
+	left := 0
+	right := len(nums) - 1
+
+	for left < right {
+		if nums[left]%2 == 0 {
+			left++
+		} else if nums[right]%2 == 1 {
+			right--
 		} else {
-			odds = append(odds, v)
+			nums[left], nums[right] = nums[right], nums[left]
+			left++
+			right--
 		}
 	}
-	return append(evens, odds...)
+	return nums
 
 }
