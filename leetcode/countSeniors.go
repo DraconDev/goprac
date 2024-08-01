258ab9117096b902292b5241e483ef290aaa3ae6
DraconDev <dracsharp@gmail.com>
2024-08-01 05:35:29 +0100

    add

---
+++ leetcode/countSeniors.go
@@ -0,0 +1,14 @@
+package leetcode
+
+func countSeniors(details []string) int {
+	old := 0
+	for _, v := range details {
+		if v[11] >= '6' && v[12] >= '1' {
+			old++
+		} else if v[11] >= '7' {
+			old++
+		}
+	}
+	return old
+
+}
